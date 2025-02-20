cdfa4070cc4705d2b9cc96c4f5440bae1c8fead8
vishal-Tamhane <vishaltamhane367@gmail.com>
2025-02-20 22:34:28 +0530

    changes in a navbar and makes some responsive

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const navbar = document.querySelector('nav'); // Adjust selector if necessary
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) { // When scrolled 50px or more
+    navbar.classList.add('scrolled');
+  } else { // When back at the top
+    navbar.classList.remove('scrolled');
+  }
+});
+
+
+
