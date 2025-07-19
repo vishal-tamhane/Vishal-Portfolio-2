6b3113f3cc12fd5d8375513960fcb8d861d80c6d
vishal-Tamhane <vishaltamhane367@gmail.com>
2025-07-19 19:50:51 +0530

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const navbar = document.querySelector('nav'); // Adjust selector if necessary
 
 window.addEventListener('scroll', () => {
@@ -8,6 +6,17 @@ window.addEventListener('scroll', () => {
   } else { // When back at the top
     navbar.classList.remove('scrolled');
   }
+});
+
+window.addEventListener('scroll', function() {
+    const socialIcons = document.querySelector('#header .social-icons');
+    const headerHeight = document.querySelector('#header').offsetHeight;
+    
+    if (window.scrollY > headerHeight - 100) {
+        socialIcons.classList.add('hidden');
+    } else {
+        socialIcons.classList.remove('hidden');
+    }
 });
 
 
